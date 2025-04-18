bc22c98bda116ade1fc53cb03eb127aa87f00259
Mikołaj Kokot <panmeek@proton.me>
2025-04-18 18:09:39 +0200

    Add solution for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+// I feel bad about this after looking at the solutions T_T
+
+const removeFromArray = function(array, ...items) {
+    for (item of items) {
+        let filteredItems = array.filter((itemFilter) => itemFilter === item);
+        for (item of filteredItems) {
+            array.splice(array.indexOf(item), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
