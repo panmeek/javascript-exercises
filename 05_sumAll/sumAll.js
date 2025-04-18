b165105fb26aaa4c91b4eba3dbcbf1b7b1bad7fc
Mikołaj Kokot <panmeek@proton.me>
2025-04-18 18:33:47 +0200

    Add solution for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let startNum, endNum, result = 0;
 
+    if (
+        !Number.isInteger(num1) ||
+        !Number.isInteger(num2) ||
+        num1 < 0 ||
+        num2 < 0
+    ) {
+        return "ERROR"
+    }
+
+    if (num1 > num2) {
+        startNum = num2;
+        endNum = num1;
+    } else {
+        startNum = num1;
+        endNum = num2;
+    }
+
+    for (let i = startNum; i <= endNum; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
